684b98b6b26c005f0c8c73e88f130267a99280e0
Bezael Pérez <bezael_e@hotmail.com>
2018-05-19 18:33:19 +0200

    Menu full

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function cambiarClase(){
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    let menuOpen = document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');    
+        
+}
